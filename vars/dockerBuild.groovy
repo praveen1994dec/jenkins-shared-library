588e686bd513e077038a903c9e625f972798c8c4
Praveen Singampalli <praveensingampalli1994@gmail.com>
2023-03-18 16:04:10 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -3,7 +3,7 @@ def call(String project, String hubUser) {
     sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}"
     sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:latest"
     withCredentials([usernamePassword(
-            credentialsId: "docker",
+            credentialsId: "docker_cred",
             usernameVariable: "USER",
             passwordVariable: "PASS"
     )]) {
